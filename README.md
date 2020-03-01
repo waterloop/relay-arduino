ed1a7ad2015ff5992191613b411163002f5e4c7c
Clive Chan <cc@clive.io>
2020-03-01 16:28:03 -0500

    add the right file, add to readme

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Relay Arduino for communicating in between sensors and desktop.
 
 Libraries to install in Arduino IDE:
 - CAN by Sandeep Mistry
+- Ethernet and SD libraries are already provided by default.
+
+## UDPCANRelay + CANEcho
+Input: `stdbuf -o0 nc -u 192.168.0.9 2323`
+Output: `stdbuf -o0 nc -lu 192.168.0.6 2323`
+
+### Note on SPI
+SPI CS pin conflicts are possible when you're using multiple SPI libraries (i.e. CAN, Ethernet, SD). The code in UDPCANRelay is written to not keep any SPI connection open, which hopefully causes the library to disable the corresponding CS pin.
+
+- i.e. if you keep TCP connection alive the library might be keeping the CS pin active. But not sure how this works exactly.
--- UDPCANRelay/UDPCANRelay.ino
+++ UDPCANRelay/UDPCANRelay.ino
@@ -1,73 +1,82 @@
-// This Arduino will repeatedly send CAN packets! Can receive them as well.
-// Used for testing the main Relay Arduino. 
-
-#include <CAN.h>
-#include <SD.h>
 #include <SPI.h>
+#include <SD.h>
+#include <Ethernet.h>
+#define protected public // I'm sorry
+#include <CAN.h>
 
 int BUFFER_SIZE_CAN = 8;
-File packetLog;
+int BUFFER_SIZE_TCP = 8;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+IPAddress relayip(192, 168, 0, 9);
+int relayport = 2323;
+IPAddress desktopip(192, 168, 0, 255);
+int desktopport = 2323;
+
+unsigned char buf[UDP_TX_PACKET_MAX_SIZE];
+unsigned char sprintfbuf[24];
+
+EthernetUDP Udp;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
-
-  if (!CAN.begin(500E3)) {
-    Serial.println("Starting CAN failed.");
-  }
-
-  if (!SD.begin(4)) {
-    Serial.println("SD card init failed.");
-  }
-
-  // register callback for when CAN packet is received
-  //CAN.onReceive(onReceive);
+  while(!Serial);
+  Ethernet.begin(mac, relayip);
+  while(!Udp.begin(relayport))Serial.println("Udp.begin failed");
+  while(!CAN.begin(500E3))Serial.println("CAN.begin failed");
+  while(!SD.begin(4))Serial.println("SD.begin failed");
+  Serial.println("Successful init");
 }
 
 void loop() {
-  
-  // TODO: check signal from desktop and send to all microcontrollers
+  int packetSize = Udp.parsePacket();
+  File sdlog;
+  if(packetSize) {
+    // assert Udp.remoteIP() == desktopip
+    // assert Udp.remotePort() == desktopport
+    if (4 < packetSize && packetSize <= 12) { // note that we do not allow zero-length CAN packets
+      Udp.read(buf, UDP_TX_PACKET_MAX_SIZE);
+      uint32_t messageId = *((uint32_t*)buf);
+      
+      Serial.println("received udp");
+      Serial.println(messageId);
+      if (messageId >= (1<<11)) {
+        Serial.println("WARNING: message id too big, truncating");
+        messageId &= (1<<11) - 1;
+      }
+      Serial.println(packetSize - 4);
+      
+      CAN.beginPacket(messageId);
+      CAN.write(buf + 4, packetSize - 4);
+      CAN.endPacket();
 
-  // if packet is received from desktop, send can packet to controllers
-  
-  // save packet to sd card
-  packetLog = SD.open("packetLog.txt", FILE_WRITE);
-  if (packetLog) {
-      packetLog.println("TCP packet: ");
-      packetLog.close();
-  } else {
-    Serial.println("Error opening packetLog.txt");
+      sdlog = SD.open("log.txt", FILE_WRITE); // Filename must be <= 8 characters long.
+      sdlog.write("UDP ");
+      sdlog.write(buf, packetSize);
+      sdlog.write('\n');
+      Serial.println("Wrote UDP to SD card!!");
+      sdlog.close();
+    } else {
+      Serial.println("received udp bad size");
+    }
   }
 
-  char dataBuffer[] = "testing!";
-
-  // create CAN packet
-  Serial.println("Starting CAN packet");
-  CAN.beginPacket(0x12);
-  CAN.write(dataBuffer, BUFFER_SIZE_CAN);
-  CAN.endPacket();
-  Serial.println("Sent CAN Packet");
-  delay(1000);
-}
-
-// if packet is received from controller, read data and send tcp to desktop
-void onReceive() {
-  Serial.println("Received CAN packet of size " + CAN.parsePacket());
-  
-  String data;
- 
-  while (CAN.available()) {
-    char c = (char)CAN.read();
-    data += c;
-  }
-  Serial.println("From CAN: " + data);
-     
-  // save packet to sd card
-  packetLog = SD.open("packetLog.txt", FILE_WRITE);
-  if (packetLog) {
-    packetLog.println("CAN packet: ");
-    packetLog.close();
-  } else {
-    Serial.println("Error opening packetLog.txt");
+  packetSize = CAN.parsePacket();
+  if(packetSize) {
+    Serial.println("received can");
+    
+    Udp.beginPacket(desktopip, desktopport);
+    uint32_t packetId = (uint32_t)CAN.packetId();
+    Udp.write((uint8_t*)&packetId, 4);
+    Udp.write(CAN._rxData, packetSize);
+    Udp.endPacket();
+    
+    sdlog = SD.open("log.txt", FILE_WRITE); // Filename must be <= 8 characters long.
+    sdlog.write("CAN ");
+    sdlog.write((uint8_t*)&packetId, 4);
+    sdlog.write((uint8_t*)CAN._rxData, packetSize);
+    sdlog.write('\n');
+    Serial.println("Wrote CAN to SD card!!");
+    sdlog.close();
   }
 }
